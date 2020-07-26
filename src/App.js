e4645d1a99109efc9e1452b8f5d001478a3af12f
Abiramikarnan <abiramikarnan1999@gmail.com>
2020-07-26 19:58:07 +0530

    log in page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Login from './login';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
